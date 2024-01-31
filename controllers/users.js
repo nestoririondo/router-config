204464e962eea0f3b8291ddfdf082e0a2754cd2d
Néstor Iriondo <nestoririondo@gmail.com>
2024-01-31 13:01:08 +0100

    checkUser middleware

--- controllers/users.js
+++ controllers/users.js
@@ -1,33 +1,5 @@
 import pool from "../db/pool.js";
-import { body, validationResult } from "express-validator";
-
-export const userValidator = () => [
-  body("first_name")
-    .notEmpty()
-    .withMessage("First name required.")
-    .isString()
-    .withMessage("First name must be a string.")
-    .trim()
-    .escape(),
-  body("last_name")
-    .notEmpty()
-    .withMessage("Last name required.")
-    .isString()
-    .withMessage("Last name must be a string.")
-    .trim()
-    .escape(),
-  body("age")
-    .notEmpty()
-    .withMessage("Age required.")
-    .isInt({ min: 1 })
-    .withMessage("Age must be a number greater than 0."),
-];
-
-export const validatePutUser = () => [
-  body("first_name").optional().notEmpty().isString().trim().escape(),
-  body("last_name").optional().notEmpty().isString().trim().escape(),
-  body("age").optional().notEmpty().isInt({ min: 0 }).trim().escape(),
-];
+import { validationResult } from "express-validator";
 
 export const getUsers = async (req, res) => {
   console.log("GET users");
@@ -42,15 +14,12 @@ export const getUsers = async (req, res) => {
 
 export const getUser = async (req, res) => {
   const { id } = req.params;
-  if (!id) res.status(400).json("User id required.")
   console.log(`GET user ${id}`);
   try {
     const text = "SELECT * FROM users WHERE id=$1";
     const values = [id];
     const { rows } = await pool.query(text, values);
-    rows.length === 0
-      ? res.status(404).json("User not found")
-      : res.status(200).json(rows[0]);
+    res.status(200).json(rows[0]);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -114,9 +83,7 @@ export const putUser = async (req, res) => {
 
     const { rows } = await pool.query(text, values);
 
-    rows.length === 0
-      ? res.status(404).json("User not found")
-      : res.status(200).json(rows[0]);
+    res.status(200).json(rows[0]);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -124,15 +91,12 @@ export const putUser = async (req, res) => {
 
 export const deleteUser = async (req, res) => {
   const { id } = req.params;
-  if (!id) res.status(400).json("User id required.");
   console.log(`DELETE user ${id}`);
   try {
     const text = "DELETE FROM users WHERE id = $1 RETURNING *";
     const values = [id];
     const { rows } = await pool.query(text, values);
-    rows.length === 0
-      ? res.status(404).json("User not found")
-      : res.status(200).json(rows[0]);
+    res.status(200).json(rows[0]);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { getUsers, getUser, postUser, putUser, deleteUser, userValidator, validatePutUser } from '../controllers/users.js'
+import { getUsers, getUser, postUser, putUser, deleteUser } from '../controllers/users.js'
+import { userValidator, validatePutUser, checkUser } from '../middlewares/users.js'
 
 const usersRouter = express.Router();
 
 usersRouter.get("/", getUsers)
-usersRouter.get("/:id", getUser)
+usersRouter.get("/:id", checkUser, getUser)
 usersRouter.post("/", userValidator(), postUser)
-usersRouter.put("/:id", validatePutUser(), putUser)
-usersRouter.delete("/:id", deleteUser)
+usersRouter.put("/:id", checkUser, validatePutUser(), putUser)
+usersRouter.delete("/:id", checkUser, deleteUser)
 
 export default usersRouter;
